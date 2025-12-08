c90d00e0b7444e1021659a394f8526b91a9a2f71
unknown <acosta.herrera.oliver@gmail.com>
2025-12-08 13:07:27 -0400

    added adt winform

---
+++ NCQ-ADT/TaskEditor.cs
@@ -0,0 +1,50 @@
+﻿
+
+using Infrastructure.Dal;
+
+namespace NCQ_ADT
+{
+    public partial class TaskEditor : Form
+    {
+        public TaskEditor()
+        {
+            InitializeComponent();
+            initUI();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void initUI()
+        {
+            var db = new DbContext();
+            var combos = GetAllCombos(this);
+            foreach (var item in combos)
+            {
+                item.DisplayMember = "Name";
+                item.ValueMember = "Id";
+            }
+            //usuarios
+            this.cbUsers.DataSource = db.userRepository.All();
+            //status
+            this.cbStatus.DataSource = db.statusRepository.All();
+            //prioridad
+            this.cbPriority.DataSource = db.prioritiesRepository.All();
+            //fecha
+        }
+
+        public IEnumerable<ComboBox> GetAllCombos(Control parent)
+        {
+            foreach (Control c in parent.Controls)
+            {
+                if (c is ComboBox combo)
+                    yield return combo;
+
+                foreach (var child in GetAllCombos(c))
+                    yield return child;
+            }
+        }
+    }
+}
